585a9ee8af1fb805d9fe793ddd4dd155a6f6caca
Celso Marciano <cemarte83@gmail.com>
2017-05-09 15:59:35 -0400

    add patient query patient

--- src/experiments/elasticSearch/nestBit/Program.cs
+++ src/experiments/elasticSearch/nestBit/Program.cs
@@ -1,14 +1,34 @@
 ﻿using Nest;
 using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.InteropServices;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
+using Newtonsoft.Json;
 
 namespace nestBit
 {
     class Program
     {
+        public static IConfigurationRoot Configuration { get; set; }
+
+
+
         private static ElasticClient Client { get; set; }
 
         static void Main(string[] args)
         {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appSettings.json");
+
+            Configuration = builder.Build();
+
+            string url = Configuration["elasticSearch:url"];
+            string elasticSearchIndex = Configuration["elasticSearch:index"];
+            string userName = Configuration["elasticSearch:userName"];
+            string password = Configuration["elasticSearch:password"];
             // add some files
             // add some patients 
             // put some stuff in it.
@@ -16,10 +36,49 @@ namespace nestBit
             // simple search
             // full text search 
             // isolation by tenantId
+            var node = new Uri(url);
+            var settings = new ConnectionSettings(node);
+            settings.BasicAuthentication(userName, password);
+
+            var client = new ElasticClient(settings);
+
+            var patient = new Patient
+            {
+                Id = 1,
+                FirstName = "Mark",
+                LastName = "Zuckerberg",
+                AlternateIdentifiers = new List<AlternateIdentifier> { new AlternateIdentifier()
+                    {
+                        Code= "MRN",
+                        Value="123-456-MMNNNRR"
+                    }
+                }
+            };
+            var response = client.Index(patient, idx => idx.Index(elasticSearchIndex));
+            Console.WriteLine(JsonConvert.SerializeObject(response));
+
+            var patientResult = client.Get<Patient>(response.Id, idx => idx.Index(elasticSearchIndex));
+            Console.WriteLine(JsonConvert.SerializeObject(patientResult));
 
 
-
-            Console.WriteLine("Hello World!");
+            Console.ReadKey(false);
         }
+
+
+    }
+
+    internal class Patient
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public List<AlternateIdentifier> AlternateIdentifiers { get; set; }
+    }
+
+    internal class AlternateIdentifier
+    {
+        public int Id { get; set; }
+        public string Code { get; set; }
+        public string Value { get; set; }
     }
 }
