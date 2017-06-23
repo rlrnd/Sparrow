8b366ad60330afb8f0ae89dbee98f00a8967b503
Jian <jchonc@gmail.com>
2017-06-23 09:59:34 -0400

    partial join path should be fine now

--- src/experiments/ractForm/src/utils/index.js
+++ src/experiments/ractForm/src/utils/index.js
@@ -4,14 +4,25 @@ import {compose, getContext} from 'recompose';
 
 export function combinePath(basePath, valuePath) {
     if(!basePath) return valuePath;
-    let simpPath = basePath.replace(/\[\d\]/g, "");
-    if (valuePath.startsWith(simpPath)) {
-        valuePath = valuePath.substring(simpPath.length);
-        return basePath+valuePath;
+    let baseParts = basePath.split('.');
+    let valueParts = valuePath.split('.');
+    let results = [];
+
+    for(let i = 0; i < baseParts.length; i++) {
+        let basePart = baseParts[i].replace(/\[\d\]/g, "");
+        let valuePart = valueParts[0];
+        if(basePart != valuePart) 
+        {
+            break;
+        }
+        results.push(baseParts[i]);
+        valueParts.splice(0,1);
+        if(valueParts.length == 0) {
+            break;
+        }
     }
-    else {
-        return valuePath;
-    }
+    results.push(...valueParts);
+    return results.join(".");
 }
 
 export function connectWithPath(...args) {
